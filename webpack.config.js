f036ed4abb4437c34d82df50ed91484f426611a1
SharkSavvy <jimmyslagle@gmail.com>
2025-02-25 14:39:30 -0500

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/ui/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'ui'),
+    filename: 'app.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                '@babel/preset-react',
+                '@babel/preset-typescript'
+              ]
+            }
+          }
+        ],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    fallback: {
+      "path": require.resolve("path-browserify"),
+      "os": require.resolve("os-browserify/browser"),
+      "crypto": require.resolve("crypto-browserify")
+    }
+  },
+  performance: {
+    hints: false
+  }
+};
